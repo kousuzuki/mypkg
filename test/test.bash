8aaaa38ed9372b3d260f455f36e39ce8a5552a68
Kou Suzuki <pinoniq123@gmail.com>
2024-01-05 17:02:34 +0900

    rewrite test,bash

--- test/test.bash
+++ test/test.bash
@@ -10,10 +10,20 @@ colcon build
 source $dir/.bashrc
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
+
+for ((i=0; i<=15; i++)); do
+	if ! grep -q "Listen: $i" /tmp/mypkg.log; then
+	    echo "Test Failed: Expected output 'Listen: $i' not found."
+	    exit 1
+	 fi
+ done	 
+
+ echo "The count has reached 15"
+
+
 if grep -q 'Listen: 16' /tmp/mypkg.log; then
     echo "Test Failed: Expected output not found."
     exit 1
 else
     echo "Test Passed: Expected output  found."
 fi
-
